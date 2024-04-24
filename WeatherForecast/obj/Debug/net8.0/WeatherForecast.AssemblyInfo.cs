fe618cc86d2626c30cdedc87e257de82b30fd9f0
AglionOlio <lukaszolejniczak434@interia.eu>
2024-04-24 13:53:43 +0200

    fire pipeline test

--- WeatherForecast/obj/Debug/net8.0/WeatherForecast.AssemblyInfo.cs
+++ WeatherForecast/obj/Debug/net8.0/WeatherForecast.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherForecast")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c1f199413e20b2afc7e1e37837496d48c56090a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherForecast")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherForecast")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
